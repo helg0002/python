82a18dd9ede3b2431100df4ee01685efc0439b0c
Олег <oselasnikov@gmail.com>
2022-08-16 23:14:14 +0400

    123

--- app.py
+++ app.py
@@ -1,47 +1,28 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, g
+import sqlite3
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////php_getRate/PHP/rate.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
+DATABASE = 'C:/Users/Олег/PycharmProjects/pythonProject2/php_getRate/PHP/rate.db'
 
+con = sqlite3.connect("test.db")
+cur = con.cursor()
 
-class Article(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    intro = db.Column(db.String(300), nullable=False)
-    text = db.Column(db.Text, nullable=False)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Article %r>' % self.id
-
+cur.execute('''CREATE TABLE stocks 
+(date text, trans text, symbol text, qty real, price real) ''')
+cur.execute("INSERT INTO stocks VALUES ('2006-01-05', 'BUY', 'RHAT', 100,35/14)")
+con.commit()
+con.close()
 
 @app.route('/')
 @app.route('/home')
 def index():
     return render_template("index.html")
 
-
-@app.route('/create-article', methods=['POST', 'GET'])
-def create_article():
-    if request.method == "POST":
-        title = request.form['title']
-        intro = request.form['intro']
-        text = request.form['text']
-
-        article = Article(title=title, intro=intro, text=text)
-
-        try:
-            db.session.add(article)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return "При добавлении статьи произошла ошибка"
-    else:
-        return render_template("create-article.html")
 
 
 if __name__ == "__main__":
